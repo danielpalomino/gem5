4abef99c907685f9fff3b9dfeb3139326a05cdbd
Daniel Palomino <dmvpalomino@inf.ufrgs.br>
2014-04-24 16:31:33 -0300

    Adding ces265 scripts.

---
+++ ces265_scripts/ces265_2c.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+runbench()
+{
+BENCH=$1
+CORES=$2
+	(
+		echo ${BENCH}${CORES};
+		time nice -n10 ./build/ALPHA/gem5.opt -m -d ./parsec/${BENCH}${CORES} ./configs/example/fs.py -n $CORES --cpu-type=timing --caches --l2cache --script=./parsec-scripts/${BENCH}_${CORES}c_simmedium.rcS 
+	)
+
+}
+
+for j in ces265
+do
+	runbench $j 2 &
+	wait
+done
+
