d1eccc67a1490c3b1372bad0e8feaa734f152bc1
Daniel Palomino <dmvpalomino@inf.ufrgs.br>
2014-05-12 14:52:28 -0300

    removing nice command from scripts.

--- ces265_scripts/ces265_2c.sh
+++ ces265_scripts/ces265_2c.sh
@@ -6,7 +6,7 @@ BENCH=$1
 CORES=$2
 	(
 		echo ${BENCH}${CORES};
-		time nice -n10 ./build/ALPHA/gem5.opt -m -d ./daniel/${BENCH}${CORES} ./configs/example/fs.py -n $CORES --cpu-type=detailed --caches --l2cache --script=./daniel-scripts/${BENCH}_${CORES}c_simmedium.rcS --disk-image=image_ces265.img 
+		time ./build/ALPHA/gem5.opt -m -d ./daniel/${BENCH}${CORES} ./configs/example/fs.py -n $CORES --cpu-type=detailed --caches --l2cache --script=./daniel-scripts/${BENCH}_${CORES}c_simsmall.rcS --disk-image=image_ces265.img 
 	)
 
 }
