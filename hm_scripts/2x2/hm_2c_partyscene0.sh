630ce921045f6a7fb59c73c90b1bd528406b456e
Daniel Palomino <dmvpalomino@inf.ufrgs.br>
2015-07-27 16:15:06 -0300

    adjusting party scene scripts

--- hm_scripts/2x2/hm_2c_partyscene0.sh
+++ hm_scripts/2x2/hm_2c_partyscene0.sh
@@ -6,7 +6,7 @@ BENCH=$1
 CORES=$2
 	(
 		echo ${BENCH}${CORES};
-		time ./build/ALPHA/gem5.opt -m -d ./daniel/${BENCH}${CORES}_racehorses0 ./configs/example/fs.py -n $CORES --cpu-type=detailed --caches --l2cache --script=./daniel-scripts/2x2/${BENCH}_${CORES}c_simmedium_partyscene0.rcS --disk-image=image_ces265.img 
+		time ./build/ALPHA/gem5.opt -m -d ./daniel/${BENCH}${CORES}_partyscene0 ./configs/example/fs.py -n $CORES --cpu-type=detailed --caches --l2cache --script=./daniel-scripts/2x2/${BENCH}_${CORES}c_simmedium_partyscene0.rcS --disk-image=image_ces265.img 
 	)
 
 }
